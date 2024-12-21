c798b60d495999c067c8e5a1b8db1316eb65e9c5
Roberval Fernandes <robervalfernandesdigital@outlook.com>
2024-12-21 15:12:34 -0300

    Criando regex dos parametros.

--- Aulas/01-Fundamentos-nodejs/src/routes.js
+++ Aulas/01-Fundamentos-nodejs/src/routes.js
@@ -1,12 +1,14 @@
+import path from 'node:path';
 import { DataBase } from './database.js';
 import { randomUUID } from 'node:crypto';
+import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new DataBase();
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath ('/users'),
     handler: (req, res) => {
       const users = database.select('users');
 
@@ -17,7 +19,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath ('/users'),
     handler: (req, res) => {
       const { name, email } = req.body;
 
@@ -32,4 +34,11 @@ export const routes = [
       return res.writeHead(201).end(); // Retorna código 201 Created
     },
   },
+  {
+    method: 'DELETE',
+    path: buildRoutePath ('/users/:id'),
+    handler: (req, res) => {
+        return res.end()
+    }
+  }
 ];
